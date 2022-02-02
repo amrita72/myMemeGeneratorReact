cb44167e972b0ab3ee289b2760061aaff34d5b72
amrita72 <62096821+amrita72@users.noreply.github.com>
2022-02-02 14:12:28 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Header() {
+  return (<header>
+    <img
+      src= 'https://i.imgflip.com/3i7p.jpg'
+      alt='Problem?'
+    />
+    <p>Meme Generator</p>
+  </header>);
+}
+export default Header;
